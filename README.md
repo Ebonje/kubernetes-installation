890ba13f213bb79bdeed8794ba0a5b2f7b09cf1c
ewesum <dieuewe@yahoo.com>
2025-02-11 11:54:35 +0100

    this is the file for the installation kubeadm

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kubernetes
+
+Master.sh script is use to install kubeadm, kubectl and kubelet in New VM of Ubuntu
+
+Step1 :- Clone the Repo
+
+Step2 :- sudo chmod 777 ./master.sh
+
+Step3 :- ./master.sh
+
+You have manually install kubeadm init follow the script i mention it
+
+
+Worker.sh script can run only on worker nodes
+
---
+++ master.sh
@@ -0,0 +1,275 @@
+    ┌──────────────────────────────────────────────────────────────────────┐
+    │                 • MobaXterm Personal Edition v23.1 •                 │
+    │               (SSH client, X server and network tools)               │
+    │                                                                      │
+    │ ⮞ Linux distribution: Default                                      │
+    │ ⮞ Windows drives are mounted into /mnt path (by default)             │
+    │ ⮞ Your DISPLAY is set to 192.168.1.5:0.0                             │
+    │ ⮞ For more info, ctrl+click on help or visit our website.            │
+    └──────────────────────────────────────────────────────────────────────┘
+
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$ ls
+2027                  EKS-Complete     apache-maven-3.9.4  create_file.sh              ingress-nginx            make-up-class             project-2025
+2048                  File.txt         app                 cronjob.yaml                ingress.yaml             microservices-demo        project-app
+244                   Grafana          app-blue.yaml       delete.sh                   init-tester.yaml         minikube-linux-amd64      prometheus
+Blackbox              Jenkins          app-green.yaml      developing-with-docker      java-maven-app           morning_class             react-nodejs-example
+Boardgame             Jenkins-project  app1                diveintoansible-lab         java-project             multi-container-app       say-hi.sh
+CLASS30               Jenkinsfile      app2                docker-compose.yaml         job.yaml                 myapp-lik.yaml            setup.sh
+Configure             LFD259           asignment           docker.sh                   k8s-demo-project         myapp-project             setupLinkerd.txt
+DevOp-engineer        LICENSE          bankApp             docker1.sh                  k8s-practice             myfile.txt                shopping
+DevOps-engineer       MyEasyShop       basic.yaml          easyregistry.yaml           key                      myprojectclass26          snap
+Diablocom             NAME             bootcamp            engineer_list               kubernetes               nexus_deploy.yaml         somefile2.txt
+Docker                SCRIPT           brokendeploy.yaml   file.txt                    kubernetes-installation  nexus_deploy_backup.yaml  sonobouy.tar.gz
+Docker-project        Traces           class26             git-project                 kubernetes_intall        ping.sh                   sonobuoy_0.56.8_linux_amd64
+Domain-review         ansible-class26  class29             helm-project                linux-work               project-1                 subject
+Domain-review-design  ansible-learn    command.txt         hiring-without-whiteboards  log.txt                  project-2                 terraform
+ewesum@DESKTOP-ALU3934:~$ ls -lrt
+total 138792
+-rw-r--r--   1 ewesum ewesum     11357 Jul  2  2022 LICENSE
+drwxr-xr-x   6 root   root        4096 Jul 26  2023 apache-maven-3.9.4
+drwxr-xr-x   4 ewesum ewesum      4096 Jan  8  2024 java-project
+drwxrwxr-x   4 ewesum ewesum      4096 Jan 30  2024 ansible-learn
+drwxr-xr-x   3 ewesum ewesum      4096 Mar 21  2024 text-project
+-rw-rw-r--   1 ewesum ewesum        71 Mar 27  2024 DevOps-engineer
+-rw-rw-r--   1 ewesum ewesum     23270 Mar 27  2024 command.txt
+-rw-rw-r--   1 ewesum ewesum         0 Mar 27  2024 File.txt
+drwxrwxr-x   3 ewesum ewesum      4096 Mar 31  2024 git-project
+drwxr-xr-x   6 ewesum ewesum      4096 Apr 25  2024 welcome-to-docker
+drwxr-xr-x  15 ewesum ewesum      4096 Apr 26  2024 kubernetes_intall
+drwxr-xr-x   4 ewesum ewesum      4096 May  8  2024 kubernetes
+drwxr-xr-x   4 ewesum ewesum      4096 May 22  2024 multi-container-app
+drwxr-xr-x   3 ewesum ewesum      4096 Jun 15  2024 project-1
+drwxr-xr-x   2 ewesum ewesum      4096 Jun 15  2024 project-2
+drwxrwxr-x   2 ewesum ewesum      4096 Aug  6  2024 NAME
+-rw-rw-r--   1 ewesum ewesum       500 Aug  6  2024 docker-compose.yaml
+-rwxr--r--   1 ewesum ewesum        49 Aug 10  2024 welcome.sh
+-rwxr--r--   1 ewesum ewesum       115 Aug 11  2024 delete.sh
+-rw-------   1 ewesum ewesum       247 Aug 11  2024 engineer_list
+-rwxr--r--   1 ewesum ewesum       327 Aug 11  2024 create_file.sh
+drwxr-xr-x   6 ewesum ewesum      4096 Aug 14 15:15 Boardgame
+drwxr-xr-x   6 ewesum ewesum      4096 Aug 20 23:00 2048
+drwxr-xr-x   4 ewesum ewesum      4096 Aug 20 23:04 hiring-without-whiteboards
+-rw-r--r--   1 ewesum ewesum       573 Aug 22 22:50 nexus_deploy.yaml
+drwxr-xr-x   3 ewesum ewesum      4096 Aug 24 23:08 bankApp
+drwxr-xr-x   3 ewesum ewesum      4096 Aug 26 11:11 MyEasyShop
+-rw-r--r--   1 ewesum ewesum      1870 Sep  2 23:58 nexus_deploy_backup.yaml
+drwxr-xr-x   3 ewesum ewesum      4096 Sep  3 23:41 shopping
+-rwxr-xr-x   1 ewesum ewesum       893 Sep  7 20:47 docker.sh
+drwxr-xr-x   3 ewesum ewesum      4096 Sep  7 22:50 myprojectclass26
+drwxr-xr-x   5 ewesum ewesum      4096 Sep 10 19:18 react-nodejs-example
+drwxr-xr-x  11 ewesum ewesum      4096 Sep 10 19:44 microservices-demo
+drwxr-xr-x   3 ewesum ewesum      4096 Sep 15 18:20 EKS-Complete
+drwxrwxr-x   7 ewesum ewesum      4096 Sep 18 19:19 k8s-demo-project
+-rwxrwxr-x   1 ewesum ewesum       408 Sep 20 22:25 ping.sh
+drwxrwxr-x   2 ewesum ewesum      4096 Oct  1 18:41 app2
+-rwxrwxr-x   1 ewesum ewesum       610 Oct  1 23:14 docker1.sh
+-rw-rw-r--   1 ewesum ewesum      3280 Oct  4 22:34 Jenkinsfile
+drwxrwxr-x   2 ewesum ewesum      4096 Oct  4 23:06 SCRIPT
+-rw-r--r--   1 ewesum ewesum     21590 Oct  6 21:11 log.txt
+drwxr-xr-x   3 ewesum ewesum      4096 Oct 17 10:31 test
+-rw-r--r--   1 root   root         107 Oct 19 21:07 basic.yaml
+drwxrwxr-x   2 ewesum ewesum      4096 Oct 20 21:27 bootcamp
+-rw-rw-r--   1 ewesum ewesum       514 Oct 23 19:52 time.py
+drwxrwxr-x   2 ewesum ewesum      4096 Oct 23 20:21 app
+drwxrwxr-x   2 ewesum ewesum      4096 Oct 26 22:49 project-app
+-rw-r--r--   1 root   root         238 Oct 29 10:49 setupLinkerd.txt
+-rw-rw-r--   1 ewesum ewesum        23 Oct 29 19:49 make-up-class
+drwxrwxr-x   2 ewesum ewesum      4096 Oct 29 19:53 DevOp-engineer
+drwxrwxr-x   3 ewesum ewesum      4096 Nov 12 00:23 helm-project
+drwxrwxr-x   3 ewesum ewesum      4096 Nov 12 21:34 Blackbox
+drwxr-xr-x   3 ewesum ewesum      4096 Nov 14 21:12 subject
+drwxr-xr-x   2 ewesum ewesum      4096 Nov 20 22:24 Diablocom
+drwxr-xr-x   3 ewesum ewesum      4096 Nov 23 21:51 Jenkins-project
+-rw-r--r--   1 root   root        3986 Nov 25 11:43 easyregistry.yaml
+drwxrwxr-x   2 ewesum ewesum      4096 Nov 25 15:54 app1
+drwxr-xr-x   2 ewesum ewesum      4096 Nov 25 23:19 Domain-review
+-rw-r--r--   1 root   root         226 Nov 26 09:49 job.yaml
+-rw-r--r--   1 root   root         562 Nov 26 11:50 cronjob.yaml
+-rw-r--r--   1 root   root         221 Nov 26 11:58 init-tester.yaml
+drwxr-xr-x   2 ewesum ewesum      4096 Nov 26 14:25 Domain-review-design
+drwxr-xr-x   3 ewesum ewesum      4096 Nov 26 21:58 myapp-project
+drwxr-xr-x   2 ewesum ewesum      4096 Nov 27 21:30 Grafana
+drwxr-xr-x   3 root   root        4096 Nov 27 21:48 LFD259
+-rwxrwxr-x   1 ewesum ewesum      4617 Nov 27 22:01 setup.sh
+drwxrwxr-x   2 ewesum ewesum      4096 Nov 27 22:02 Traces
+-rw-rw-r--   1 ewesum ewesum        35 Nov 27 22:41 myapp-lik.yaml
+drwxr-xr-x   2 ewesum ewesum      4096 Nov 29 12:45 k8s-practice
+drwxrwxr-x   2 ewesum ewesum      4096 Dec  1 19:59 asignment
+drwxr-xr-x   2 ewesum ewesum      4096 Dec  2 08:26 web-project
+-rw-r--r--   1 ewesum ewesum       482 Dec  2 09:25 ingress.yaml
+drwxr-xr-x   6 ewesum ewesum      4096 Dec  4 09:59 ingress-nginx
+-rw-r--r--   1 ewesum ewesum        12 Dec  4 15:14 say-hi.sh
+-rw-r--r--   1 ewesum ewesum         9 Dec  5 11:39 somefile2.txt
+-rw-r--r--   1 ewesum ewesum       887 Dec  5 18:16 brokendeploy.yaml
+-rw-r--r--   1 ewesum ewesum  18907081 Dec  5 18:34 sonobouy.tar.gz
+-rw-r--r--   1 ewesum ewesum  18907081 Dec  5 18:42 sonobuoy_0.56.8_linux_amd64.tar.gz
+-rw-r--r--   1 ewesum ewesum      1131 Dec  5 19:08 troubleshoot-review1.yaml
+drwxr-xr-x   2 ewesum ewesum      4096 Dec  7 20:48 ansible-class26
+-rw-r--r--   1 ewesum ewesum         0 Dec 10 14:26 myfile.txt
+-rw-rw-r--+  1 ewesum ewesum     16276 Dec 18 09:14 file.txt
+drwxr-xr-x   3 ewesum ewesum      4096 Dec 18 19:41 training
+drwxr-xr-x   5 ewesum ewesum      4096 Dec 25 19:53 244
+-rw-r--r--   1 ewesum ewesum       867 Dec 27 05:46 app-green.yaml
+-rw-r--r--   1 ewesum ewesum       867 Dec 27 06:01 app-blue.yaml
+-rw-r--r--   1 ewesum ewesum 103820392 Dec 29 17:27 minikube-linux-amd64
+drwxr-xr-x   3 ewesum ewesum      4096 Dec 29 22:23 project-2025
+drwxr-xr-x   2 ewesum ewesum      4096 Dec 30 18:04 key
+drwxr-xr-x   2 ewesum ewesum      4096 Jan  1 15:49 2027
+drwxr-xr-x   2 ewesum ewesum      4096 Jan  6 21:56 workerspace
+drwxr-xr-x   3 ewesum ewesum      4096 Jan  7 20:39 class26
+drwxr-xr-x   3 ewesum ewesum      4096 Jan  7 21:01 CLASS30
+drwxr-xr-x   3 ewesum ewesum      4096 Jan  7 22:52 class29
+drwxr-xr-x   2 ewesum ewesum      4096 Jan  8 16:36 Jenkins
+drwxr-xr-x   5 ewesum ewesum      4096 Jan 13 12:03 diveintoansible-lab
+drwxr-xr-x   5 ewesum ewesum      4096 Jan 18 22:14 java-maven-app
+drwxr-xr-x   3 ewesum ewesum      4096 Jan 19 21:48 Configure
+drwxr-xr-x   2 ewesum ewesum      4096 Jan 23 05:28 morning_class
+drwxrwxr-x   5 ewesum ewesum      4096 Jan 24 19:15 prometheus
+drwxr-xr-x   3 ewesum ewesum      4096 Jan 26 22:02 Docker-project
+drwxrwxr-x   3 ewesum ewesum      4096 Jan 28 15:22 Docker
+drwx------   6 ewesum ewesum      4096 Jan 29 18:36 snap
+drwxrwxr-x   3 ewesum ewesum      4096 Jan 29 18:50 terraform
+drwxr-xr-x   2 ewesum ewesum      4096 Jan 31 06:23 linux-work
+drwxr-xr-x   4 ewesum ewesum      4096 Feb  4 18:54 developing-with-docker
+drwxr-xr-x   4 ewesum ewesum      4096 Feb 11 11:42 kubernetes-installation
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$
+ewesum@DESKTOP-ALU3934:~$ cd kubernetes-installation/
+ewesum@DESKTOP-ALU3934:~/kubernetes-installation$
+ewesum@DESKTOP-ALU3934:~/kubernetes-installation$
+ewesum@DESKTOP-ALU3934:~/kubernetes-installation$ ls
+Minikube-Installation  README.md  master.sh  worker.sh
+ewesum@DESKTOP-ALU3934:~/kubernetes-installation$
+ewesum@DESKTOP-ALU3934:~/kubernetes-installation$ cat master.sh
+#!/bin/sh
+
+sudo apt update
+sudo apt install git
+
+### INSTALLATING NETFILTER CONFIG ####
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+lsmod | grep br_netfilter
+
+### INSTALLATING NETWORK BRIDGE ####
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+### SWAPOFF ####
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+### INSTALLATING PACKAGES DOCKER ####
+sudo apt-get update -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+### ADDING KEYRING ####
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+### ADDING KEYRING INTO SOURCES LIST ####
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+### INSTALLATING DOCKER, DOCKER-CLI ####
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+### CREATING A NEW FILE OF CGROUPDRIVER DOCKER ####
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+### RESTARTING SERVICES DOCKER/CONTAINERD ####
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+#sudo systemctl status docker
+#sudo systemctl status containerd
+
+### UPDATING AND ADDING KEYRING OF K8s ####
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+### INSTALLATING K8s ####
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sleep 5
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#sudo apt update
+#sudo apt-cache madison kubeadm | tac
+exit 1
+
+-----------------------------------------------------------------------------------------------
+### MANUALLY STEPS TO-DO ###
+
+sudo nano /etc/containerd/config.toml
+#disabled_plugins = ["cri"]
+systemctl restart containerd.service
+-----------------------------------------------------------------------------------------------
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#kubeadm token create --print-join-command
+-----------------------------------------------------------------------------------------------
+### TAINT NODES ###
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+#kubectl taint nodes --all node-role.kubernetes.io/master-
+-----------------------------------------------------------------------------------------------
+### INSTALL WEAVE NETWORK ###
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+#kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+-----------------------------------------------------------------------------------------------
+### FLANNEL NETWORK ###
+#kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+-----------------------------------------------------------------------------------------------
+### INSTALL CALICO COREDNS ###
+#kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+#kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yam
+-----------------------------------------------------------------------------------------------
+kubectl label node worker-node01  node-role.kubernetes.io/worker=worker
+
+kubectl get po -n kube-system
+kubectl get nodes
+kubectl get cs
+-----------------------------------------------------------------------------------------------
+ewesum@DESKTOP-ALU3934:~/kubernetes-installation$
+
